79539e276a5a9e41ebd61d6db99ab7c2eddb7112
MTPASLE763 On Github <zhygersamueloliquiano@gmail.com>
2026-05-20 10:22:15 +0400

    Commit 25x11

---
+++ Editor/index.js
@@ -0,0 +1,2 @@
+import {Multiselect} from '@mit-app-inventor/blockly-plugin-workspace-multiselect/src/index';
+import {multiDraggableWeakMap} from '@mit-app-inventor/blockly-plugin-workspace-multiselect/src/global';
