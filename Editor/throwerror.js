bdb36a7a308485e0bd72b782fac1af5efc251d17
MTPASLE763 On Github <zhygersamueloliquiano@gmail.com>
2026-05-26 11:49:07 +0400

    Fix blocks

--- Editor/throwerror.js
+++ Editor/throwerror.js
@@ -15,7 +15,7 @@ Blockly.common.defineBlocksWithJsonArray([{
   "inputsInline": true
 }]);
 Blockly.JavaScript['throw_error'] = function() {
-  const value_error = generator.valueToCode(block, 'error', javascript.Order.ATOMIC);
-  const code = 'throw new Error ("' + value_error + '");\n';
+  var value_error = generator.valueToCode(block, 'error', javascript.Order.ATOMIC);
+  var code = 'throw new Error ("' + value_error + '");\n';
   return code;
 };
