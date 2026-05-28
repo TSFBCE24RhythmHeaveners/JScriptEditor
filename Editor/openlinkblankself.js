5bd316ee53443d9fc97c29d6f922245968c46faa
MTPASLE763 On Github <zhygersamueloliquiano@gmail.com>
2026-05-28 14:17:13 +0400

    New block

--- Editor/openlinkblankself.js
+++ Editor/openlinkblankself.js
@@ -1,5 +1,5 @@
 Blockly.defineBlocksWithJsonArray([
- {
+{
   "type": "openlinkself",
   "message0": "open link %1 in the same tab",
   "args0": [
@@ -14,10 +14,31 @@ Blockly.defineBlocksWithJsonArray([
   "colour": 345,
   "tooltip": "",
   "helpUrl": ""
+},{
+  "type": "openlinknew",
+  "message0": "open link %1 in the new tab",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "OpenLinkURLNew",
+      "check": "String"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 345,
+  "tooltip": "",
+  "helpUrl": ""
 }])
 Blockly.JavaScript['openlinkself'] = function(block) {
   var value_openlinkurlself = Blockly.JavaScript.valueToCode(block, 'OpenLinkURLSelf', Blockly.JavaScript.ORDER_ATOMIC);
   // TODO: Assemble JavaScript into code variable.
   var code = 'window.open (' + value_openlinkurlself + ', "_self");\n';
+  return code;
+};
+Blockly.JavaScript['openlinknew'] = function(block) {
+  var value_openlinkurlnew = Blockly.JavaScript.valueToCode(block, 'OpenLinkURLNew', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'window.open (' + value_openlinkurlnew + ', "_blank");\n';
   return code;
 };
