efc0885717826ff8a422a2293a690f27f42c0bd4
MTPASLE763 On Github <zhygersamueloliquiano@gmail.com>
2026-05-28 14:40:56 +0400

    Something something new block

--- Editor/throwerror.js
+++ Editor/throwerror.js
@@ -11,6 +11,20 @@ Blockly.defineBlocksWithJsonArray([
   "previousStatement": null,
   "nextStatement": null,
   "colour": 120,
+  "tooltip": "Throws an error, and indicates the end of the script.",
+  "helpUrl": "",
+  "inputsInline": false
+},{
+  "type": "throw_error_end",
+  "message0": "throw error %1 then end",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "error"
+    }
+  ],
+  "previousStatement": null,
+  "colour": 120,
   "tooltip": "Throws an error.",
   "helpUrl": "",
   "inputsInline": false
@@ -18,6 +32,11 @@ Blockly.defineBlocksWithJsonArray([
 ]);
 Blockly.JavaScript['throw_error'] = function(block) {
   var value_error = Blockly.JavaScript.valueToCode(block, 'error', javascript.Order.ATOMIC);
-  var code = 'throw new Error ("' + value_error + '");\n';
+  var code = 'throw new Error (' + value_error + ');\n';
+  return code;
+};
+Blockly.JavaScript['throw_error_end'] = function(block) {
+  var value_error = Blockly.JavaScript.valueToCode(block, 'error', javascript.Order.ATOMIC);
+  var code = 'throw new Error (' + value_error + ');\n';
   return code;
 };
