966e4ba03870edae102a5de63659afa794685796
MTPASLE763 On Github <zhygersamueloliquiano@gmail.com>
2026-05-28 13:00:17 +0400

    your own code

---
+++ Editor/custom_code.js
@@ -0,0 +1,26 @@
+Blockly.defineBlocksWithJsonArray([{
+  "type": "custom_code",
+  "tooltip": "",
+  "helpUrl": "",
+  "message0": "your own code %1 %2",
+  "args0": [
+    {
+      "type": "field_input",
+      "name": "yourowncode",
+      "text": ""
+    },
+    {
+      "type": "input_dummy",
+      "name": ""
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 225,
+  "inputsInline": true
+}]);
+Blockly.JavaScript['custom_code'] = function(block) {
+  var your_own_code = Blockly.JavaScript.valueToCode(block, 'yourowncode', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = ' + your_own_code + ' ;
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
