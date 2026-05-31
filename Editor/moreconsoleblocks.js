60c8beb35981e618803552f170c5b4e1da3ac325
MTPASLE763 On Github <zhygersamueloliquiano@gmail.com>
2026-05-31 12:17:14 +0400

    Add two more.

--- Editor/moreconsoleblocks.js
+++ Editor/moreconsoleblocks.js
@@ -13,8 +13,7 @@ Blockly.defineBlocksWithJsonArray([
   "previousStatement": null,
   "nextStatement": null,
   "colour": 120
-},
-{
+},{
   "type": "console_error",
   "tooltip": "Prints an error in console.",
   "helpUrl": "",
@@ -23,6 +22,33 @@ Blockly.defineBlocksWithJsonArray([
     {
       "type": "input_value",
       "name": "error_text"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 120
+},{
+  "type": "console_info",
+  "tooltip": "Prints an informational message in console.",
+  "helpUrl": "",
+  "message0": "console info %1",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "info_text"
+    }
+  ],
+  "previousStatement": null,
+  "nextStatement": null,
+  "colour": 120
+},{
+  "type": "clear_console",
+  "tooltip": "Clears the console.",
+  "helpUrl": "",
+  "message0": "clear console %1",
+  "args0": [
+    {
+      "type": "input_dummy"
     }
   ],
   "previousStatement": null,
@@ -38,5 +64,14 @@ Blockly.JavaScript['console_warn'] = function(block) {
 Blockly.JavaScript['console_error'] = function(block) {
   var value_error = Blockly.JavaScript.valueToCode(block, 'error_text', Blockly.JavaScript.ORDER_ATOMIC);
   var code = 'console.error(' + value_error + ');\n';
+  return code;
+};
+Blockly.JavaScript['console_info'] = function(block) {
+  var value_info = Blockly.JavaScript.valueToCode(block, 'inform_text', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = 'console.info(' + value_info + ');\n';
+  return code;
+};
+Blockly.JavaScript['clear_console'] = function(block) {
+  var code = 'console.clear();\n';
   return code;
 };
